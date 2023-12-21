ecbf20b5728bc292ce9567d5c5354af657e11d9d
ayoub abichou <89668117+stickymouse1@users.noreply.github.com>
2023-12-21 18:35:15 +0100

    added an if and else for the currecy user have

--- main.java
+++ main.java
@@ -6,9 +6,11 @@ public class main {
         int money = Integer.parseInt(JOptionPane.showInputDialog("enter the amount of cash you would like to convert"));
         String currency = JOptionPane.showInputDialog("Write your currency(USD,EUR,TND,PND)");
         String conv = JOptionPane.showInputDialog("convert currency (USD,EUR,TND,PND)");
-        if ("EUR".equals(currency)) {
+        if ("EUR".equals(currency))
+        {
 
-        } else if ("USD".equals(currency))
+        }
+        else if ("USD".equals(currency))
         {
 
         }
@@ -16,9 +18,11 @@ public class main {
         {
 
         }
-        else if ("PND".equals(currency)) {
+        else if ("PND".equals(currency))
+        {
 
-        } else {
+        }
+        else {
             JOptionPane.showMessageDialog(null,"please enter a valid currency");
         }
     }
