ad472de9cdbdaefc9ac33945a4f2a721ba183815
ayoub abichou <89668117+stickymouse1@users.noreply.github.com>
2023-12-21 12:07:22 +0100

    made the initial message work

---
+++ main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class main {
+    public static void main (String[]args){
+        Scanner scanner = new Scanner (System.in);
+        String welcome =("Insert the amount of cash you have");
+        JOptionPane.showMessageDialog(null,welcome);
+
+    }
+}
